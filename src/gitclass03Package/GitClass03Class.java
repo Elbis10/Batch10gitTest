f7a340af22f7f31475ef7d7e8b42d7d025905f55
unknown <elbism7@gmail.com>
2021-08-11 19:37:57 -0400

    first commit of project

---
+++ src/gitclass03Package/GitClass03Class.java
@@ -0,0 +1,11 @@
+package gitclass03Package;
+
+public class GitClass03Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this nis the first commit file");
+	}
+
+}
